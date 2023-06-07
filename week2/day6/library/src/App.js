233fa92e7de5753da8da94fd761c507bd22664f0
dhollaractualize <106189949+dhollaractualize@users.noreply.github.com>
2023-06-07 17:17:42 +0200

    Update App.js

--- week2/day6/library/src/App.js
+++ week2/day6/library/src/App.js
@@ -1,47 +1,75 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Input from './components/Input';
 import Table from './components/Table';
+import BookService from './services/BookService'; // Update the path if necessary
 
 function App() {
   const [books, setBooks] = useState([]);
   const [incomingBook, setIncomingBook] = useState(null);
   
-  function onBookCreated(book) {
-    setIncomingBook(null);
-    setBooks([...books, book]);
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  async function fetchBooks() {
+    const bookService = new BookService();
+    const fetchedBooks = await bookService.fetchBooks();
+    setBooks(fetchedBooks);
+  }
+
+  async function createBook(book) {
+    const bookService = new BookService();
+    const createdBook = await bookService.createBook(book);
+    setBooks([...books, createdBook]);
+  }
+
+  async function updateBook(book) {
+    const bookService = new BookService();
+    await bookService.updateBook(book);
+    setBooks(prevBooks =>
+      prevBooks.map(prevBook => (prevBook.id === book.id ? book : prevBook))
+    );
+  }
+
+  async function deleteBook(book) {
+    const bookService = new BookService();
+    await bookService.deleteBook(book.id);
+    setBooks(prevBooks =>
+      prevBooks.filter(prevBook => prevBook.id !== book.id)
+    );
   }
 
   function onBookCreated(book) {
     setIncomingBook(null);
-    setBooks([...books, book]);
+    createBook(book);
   }
 
   function onBookDelete(book) {
-    setBooks(books.filter((x) => x.isbn !== book.isbn));
+    deleteBook(book);
   }
 
   function onBookEdit(book) {
     setIncomingBook(book);
-    setBooks(books.filter((x) => x.isbn !== book.isbn));
+    deleteBook(book);
   }
   
 
   return (
-    <div className = "container my-5">
-    <div className = "card text-center p-4">
+    <div className="container my-5">
+      <div className="card text-center p-4">
         <h1>Library</h1>
         <Input
-        incomingBook = {incomingBook}
-        onBookCreated={onBookCreated}
+          incomingBook={incomingBook}
+          onBookCreated={onBookCreated}
         ></Input>
         <Table
-        books = {books}
-        onBookEdit={onBookEdit}
-        onBookDelete={onBookDelete}>
-        </Table>
-    </div>
+          books={books}
+          onBookEdit={onBookEdit}
+          onBookDelete={onBookDelete}
+        ></Table>
+      </div>
     </div>
   );
 }
